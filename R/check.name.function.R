96da9ee247d8bfbdecded1baf1326e5423d6bff7
William Crump <williamcrump@Williams-MacBook-Pro-3.local>
2020-03-25 15:04:32 -0700

    check input type

--- R/check.name.function.R
+++ R/check.name.function.R
@@ -5,9 +5,13 @@
 #' @return A data frame with row names from the first column if the first column was not numeric or integer
 
 check.name <- function(df){
-	library(tidyverse)
-	if (class(df[,1]) != "integer" & class(df[,1]) != "numeric"){
-		df <- column_to_rownames(df, var = colnames(df[1]))
+	if(!is.data.frame(df)){
+		return(df)
+	}else{
+		library(tidyverse)
+		if (class(df[,1]) != "integer" & class(df[,1]) != "numeric"){
+			df <- column_to_rownames(df, var = colnames(df[1]))
+		}
 	}
 	return(df)
 	}

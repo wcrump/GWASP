841fc582d78476d1a44abceac5a2c47747fd06c9
William Crump <williamcrump@Williams-MacBook-Pro-3.local>
2020-03-25 19:43:02 -0700

    trying to make initial documentation for the package as a whole...hope this works

---
+++ R/GWASP.R
@@ -0,0 +1,8 @@
+#' GWASP: A package for performing GWAS by GLM using PCs as covariates.
+#'
+#' The GWASP package performs GWAS by GLM using PCs as covariates to account for population structure.
+#'
+#'
+#' @docType package
+#' @name GWASP
+NULL
